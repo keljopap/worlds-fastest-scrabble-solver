2ae5e143f9327c9c7d869939efd8a9a8111bd46d
Kelli Pape <keljopap@umich.edu>
2017-08-27 15:52:25 -0400

    create board from input file

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 scrabble.cpp -o scrabble; ./scrabble a e t i l a p e o w a y < played.txt
+g++ -std=c++11 scrabble.cpp -o scrabble; ./scrabble < played.txt
--- scrabble.cpp
+++ scrabble.cpp
@@ -30,25 +30,75 @@ void print_board() {
 void init_letter_values() {
         letter_values = {{'a', 9}, {'b', 2}, {'c', 2}, {'d', 4}, {'e', 12}, {'f', 2}, {'g', 3}, {'h', 2}, {'i', 9}, 
                          {'j', 1}, {'k', 1}, {'l', 4}, {'m', 2}, {'n', 6}, {'o', 8}, {'p', 2}, {'q', 1}, {'r', 6}, 
-                         {'s', 4}, {'t', 6}, {'u', 4}, {'v', 2}, {'w', 2}, {'x', 1}, {'y', 2}, {'z', 1}, {' ', 2}}; 
+                         {'s', 4}, {'t', 6}, {'u', 4}, {'v', 2}, {'w', 2}, {'x', 1}, {'y', 2}, {'z', 1}, {'?', 2}}; 
 }
 
 void init_tile_bag() {
         tile_bag = {{'a', 1}, {'b', 3}, {'c', 3}, {'d', 2}, {'e', 1}, {'f', 4}, {'g', 2}, {'h', 4}, {'i', 1}, 
                     {'j', 8}, {'k', 5}, {'l', 1}, {'m', 3}, {'n', 1}, {'o', 1}, {'p', 3}, {'q', 10}, {'r', 1}, 
-                    {'s', 1}, {'t', 1}, {'u', 1}, {'v', 4}, {'w', 4}, {'x', 8}, {'y', 4}, {'z', 10}, {' ', 0}};
+                    {'s', 1}, {'t', 1}, {'u', 1}, {'v', 4}, {'w', 4}, {'x', 8}, {'y', 4}, {'z', 10}, {'?', 0}};
 }
 
 void set_rack(char rack[]) {
         for (int i = 0; i < RACK_SIZE; i++) {
-                
+            if (letter_values.count(rack[i])) {
+                cout << "added to rack, value : " << rack[i] << endl;
+            }
+            else {
+                cout << "could not add to rack, value : " << rack[i] << endl;
+            }
         }
+}
+
+void add_word(int row, int col, string word, char orientation) {
+    switch(orientation) {
+        case 'a':
+            if (row + word.length() < NUM_ROWS && col < NUM_COLS) {
+
+            }
+            break;
+        case 'd':
+            if (col + word.length() < NUM_COLS && row < NUM_ROWS) {
+                for (int i = 0; i < word.length(); i++) {
+                    scrabble_board[row][i] = word[i];
+                }
+            }
+            break;
+    }
+}
+
+void set_board() {
+
+    int idx = 0;
+    cout << "Constructing Scrabble board for:      (odd squares = '_', even squares = '|')" << endl;
+    // This is where user can provide #across words followed by that #lines "{start pos} {word}",
+    //                                #down words followed by that #lines "{start pos} {word}" (.txt file)
+
+    string line;
+    int across_words = 0;
+    int down_words = 0;
+
+    getline(cin, line);
+    across_words = std::stoi(line);
+
+    while (across_words-- > 0) {
+        getline(cin, line);
+        cout << idx++ << ") " << line << endl;
+    }
+
+    getline(cin, line);
+    down_words = std::stoi(line);
+
+    while (down_words-- > 0) {
+        getline(cin, line);
+        cout << idx++ << ") " << line << endl;
+    }
+    cout << endl;
 }
 
 int main(int argc, char* argv[]) {
 
         ifstream dictionary;
-        string line;
         dictionary.open("dictionary.txt");
         if (!dictionary.is_open()) {
                 cerr << "error:  cannot open dictionary" << endl;
@@ -60,13 +110,7 @@ int main(int argc, char* argv[]) {
                 dictionary.close();
         }
 
-        int idx = 0;
-        cout << "Constructing Scrabble board for: " << endl;
-        // This is where user can provide #across words followed by that #lines "{start pos} {word}",
-        //                                #down words followed by that #lines "{start pos} {word}" (.txt file)
-        while (getline(cin, line)) {
-                cout << idx++ << ") " << line << endl;
-        }
+        set_board();
 
         // Going to use bitsets to optimize char storage (5 bits / char (instead of 8)
         bitset<M> dawg(string("1100"));
@@ -82,9 +126,9 @@ int main(int argc, char* argv[]) {
                         } else if (tile == 113) {
                                 scrabble_board[i][j] = '*';
                         } else if (tile%2==0) {
-                                scrabble_board[i][j] = 'E';
+                                scrabble_board[i][j] = '|';
                         } else {
-                                scrabble_board[i][j] = 'O';
+                                scrabble_board[i][j] = '_';
                         }
                 }
         }
